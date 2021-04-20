f8567bc49520736ccf8aaaaac704ae6905cf41cc
Geislor Crestani <geislor@gmail.com>
2021-04-20 13:35:55 -0300

    accept xml response

--- tapioca_qualys_was/__init__.py
+++ tapioca_qualys_was/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Site Blindado S.A.'
 __email__ = 'dev@siteblindado.com.br'
-__version__ = '0.8'
+__version__ = '0.9'
 
 
 from .tapioca_qualys_was import Qualys_was  # noqa
--- tapioca_qualys_was/tapioca_qualys_was.py
+++ tapioca_qualys_was/tapioca_qualys_was.py
@@ -4,7 +4,7 @@ from tapioca import (
     TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
 
 from requests.auth import HTTPBasicAuth
-
+import xmltodict
 
 from .resource_mapping import RESOURCE_MAPPING
 
@@ -14,6 +14,15 @@ class Qualys_wasClientAdapter(JSONAdapterMixin, TapiocaAdapter):
     resource_mapping = RESOURCE_MAPPING
 
     def get_request_kwargs(self, api_params, *args, **kwargs):
+        # stores kwargs prefixed with 'xmltodict_unparse__' for use by xmltodict.unparse
+        self._xmltodict_unparse_kwargs = {k[len('xmltodict_unparse__'):]: kwargs.pop(k)
+                                          for k in kwargs.copy().keys()
+                                          if k.startswith('xmltodict_unparse__')}
+        # stores kwargs prefixed with 'xmltodict_parse__' for use by xmltodict.parse
+        self._xmltodict_parse_kwargs = {k[len('xmltodict_parse__'):]: kwargs.pop(k)
+                                        for k in kwargs.copy().keys()
+                                        if k.startswith('xmltodict_parse__')}
+
         params = super(Qualys_wasClientAdapter, self).get_request_kwargs(
             api_params, *args, **kwargs)
 
@@ -24,8 +33,7 @@ class Qualys_wasClientAdapter(JSONAdapterMixin, TapiocaAdapter):
             params['headers'] = {}
 
         params['headers'].update({
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'})
+            'Content-Type': 'application/json'})
 
         return params
 
@@ -35,6 +43,14 @@ class Qualys_wasClientAdapter(JSONAdapterMixin, TapiocaAdapter):
     def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
                                          response_data, response):
         pass
+
+    def response_to_native(self, response):
+        if response.content.strip():
+            if 'xml' in response.headers['content-type']:
+                return xmltodict.parse(response.content, **self._xmltodict_parse_kwargs)
+            elif 'json' in response.headers['content-type']:
+                return response.json()
+        return {'text': response.text}
 
 
 Qualys_was = generate_wrapper_from_adapter(Qualys_wasClientAdapter)
